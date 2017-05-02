6aea4c385e13a00fe2fd63ad87425f8a43f0993b
Michael-Yan <956462326@qq.com>
2017-05-02 17:20:34 +0800

    Site updated: 2017-05-02 17:20:33

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue May 02 2017 17:18:45 GMT+0800 (CST)
+// build time:Tue May 02 2017 17:20:20 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
